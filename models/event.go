a082314e6204987d326706445c7ba3d162a673d0
dactoankmapydev <nguyendactoankma@gmail.com>
2022-05-14 01:05:11 +0700

    edit models

--- models/event.go
+++ models/event.go
@@ -4,10 +4,11 @@ package models
 type Events struct {
 	Events []Event `json:"events,omitempty"`
 }
+
 // Event ...
 type Event struct {
-	SessionID string      `json:"session_id,omitempty"`
-	Type      int64       `json:"type,omitempty"`
-	Data      interface{} `json:"data,omitempty"`
-	Timestamp int64       `json:"timestamp,omitempty"`
+	SessionID string      `json:"session_id"`
+	Type      int64       `json:"type"`
+	Data      interface{} `json:"data"`
+	Timestamp int64       `json:"timestamp"`
 }
--- models/session.go
+++ models/session.go
@@ -2,20 +2,14 @@ package models
 
 // Session ...
 type Session struct {
-	SessionID      string  `json:"session_id,omitempty"`
-	ClientID       string  `json:"client_id,omitempty"`
-	SessionName    string  `json:"session_name,omitempty"`
-	Events         []Event `json:"events,omitempty"`
-	UserAgent      string  `json:"user_agent,omitempty"`
-	Browser        string  `json:"browser,omitempty"`
-	Version        string  `json:"version,omitempty"`
-	BrowserVersion string  `json:"browser_version,omitempty"`
-	OS             string  `json:"os,omitempty"`
-	Device         string  `json:"device,omitempty"`
-	ScreenSize     string  `json:"screen_size,omitempty"`
-	Duration       string  `json:"duration,omitempty"`
-	PageLoadTime   uint64  `json:"page_load_time,omitempty"`
-	Source         string  `json:"source,omitempty"`
-	CreatedAt      string  `json:"created_at,omitempty"`
-	UpdatedAt      string  `json:"updated_at,omitempty"`
+	ID        string  `json:"session_id"`
+	WebsiteID string  `json:"website_id"`
+	Name      string  `json:"name"`
+	UserAgent string  `json:"user_agent"`
+	Browser   string  `json:"browser"`
+	Version   string  `json:"version"`
+	OS        string  `json:"os"`
+	CreatedAt string  `json:"created_at"`
+	UpdatedAt string  `json:"updated_at"`
+	Events    []Event `json:"events"`
 }
