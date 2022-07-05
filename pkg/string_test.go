23263cd359b0cd39696447823ab50f894eaa18d9
dactoankmapydev <nguyendactoankma@gmail.com>
2022-07-06 01:35:27 +0700

    test remove sub string

--- pkg/string_test.go
+++ pkg/string_test.go
@@ -2,12 +2,32 @@ package pkg
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func TestString(t *testing.T) {
-	expectedResult := "hello"
-	receiveResult := RemoveSubstring("hello girl", "girl")
-	assert.Equal(t, expectedResult, receiveResult)
+func TestRemoveSubstring(t *testing.T) {
+	type args struct {
+		s      string
+		substr string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "should remove sub string from string",
+			args: args{
+				s:      "hello girl, my name is x",
+				substr: "x",
+			},
+			want: "hello girl, my name is",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := RemoveSubstring(tt.args.s, tt.args.substr); got != tt.want {
+				t.Errorf("RemoveSubstring() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
